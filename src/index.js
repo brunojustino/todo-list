8d99f75f083c1da021f152505e771d5d3810eabd
brunojustino <brunojustino@yahoo.com.br>
2021-06-24 18:05:59 -0300

    started layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import { productsContent } from './products'
 // import vera from './img/vera.jpg'
+import "./css/styles.css";
 
 console.log("hi")
 let content = document.getElementById("content")
 
-content.innerHTML = "alalalalal"
