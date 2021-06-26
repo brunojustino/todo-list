bf58a93afe088ae9a1682dcbffd1c0468ffb9fcc
brunojustino <brunojustino@yahoo.com.br>
2021-06-26 00:42:52 -0300

    Almost finished design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import { productsContent } from './products'
 // import vera from './img/vera.jpg'
 import "./css/styles.css";
+import "@fortawesome/fontawesome-free/js/all.js"
 
 console.log("hi")
 let content = document.getElementById("content")
